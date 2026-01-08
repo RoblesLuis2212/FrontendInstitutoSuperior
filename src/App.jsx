93fa580bb715b2513e96b8bab9a4730388b92784
Luis Robles <luisgeremiasrobles@gmail.com>
2026-01-08 11:15:16 -0300

    Agregue botones de acciones como agregar, eliminar y editar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./components/Footer";
 import TablaAlumnos from "./components/TablaAlumnos";
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
   return (
     <>
-      <main>
-        <h1 className="text-center mt-3">Administracion de Alumnos</h1>
+      <main className="container">
+        <div className="d-flex justify-content-center align-items-center gap-3 p-3">
+          <h1 className="text-center mt-2 flex-grow-1">Administracion de Alumnos</h1>
+          <Button variant="success me-1">Agregar</Button>
+        </div>
         <TablaAlumnos></TablaAlumnos>
       </main>
       <Footer></Footer>
